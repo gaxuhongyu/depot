59078290ca4b7a88636cede141aaac6ac39946c1
gaxuhongyu <gaxuhongyu@gmail.com>
2012-08-21 16:45:52 +0800

    add aome test case.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :descrption, :image_url, :price, :title
+  validates :title, :descrption, :image_url, :presence => true #表示不能为空
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01} 
+  validates :title, :uniqueness => true #表示独一无二
+  validates :image_url, :format => {
+    :with     => %r{\.(gif|jpg|png)$}i,
+    :message  => 'must be aURL for GIF,JPG or PNG image.'
+  } #表示图片格式要求
 end
